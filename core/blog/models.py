a32ced3849f8844e38ea053c24115d2530288816
mehranniakan <65720684+mehranniakan@users.noreply.github.com>
2026-06-26 21:51:54 +0330

    create posts and category table

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,23 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
