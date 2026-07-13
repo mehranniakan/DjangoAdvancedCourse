142255ab08516c4f8cd47bcdb424481bc5baa903
mehranniakan <65720684+mehranniakan@users.noreply.github.com>
2026-07-13 17:33:59 +0330

    add db conf to docker compose and change docker-image

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM python:3.8-slim-buster
+FROM python:3.8-bullseye
 ARG app_path=/app
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 WORKDIR ${app_path}
 COPY requirements.txt /${app_path}
-RUN pip3 install --upgrade pip
-RUN pip3 install -r requirements.txt
+RUN pip install --upgrade pip
+RUN apt-get update && apt-get install -y curl
+RUN pip install -r requirements.txt
 COPY ./core /${app_path}/
 EXPOSE 8000
 CMD ["python","manage.py", "runserver", "0.0.0.0:8000"]
--- core/core/settings.py
+++ core/core/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+import decouple
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,14 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#hy=0s#tdc@t%^g&ez^)z3wy(m2)439aa9swhz#rb9oxpt_)zq"
+SECRET_KEY = config('SECRET_KEY',default='test')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    "*"
-]
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost",
+    cast=lambda h : [i.strip() for i in h.split(",")]
+)
 
 # Application definition
 
@@ -88,8 +93,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT", cast=int),
     }
 }
 
