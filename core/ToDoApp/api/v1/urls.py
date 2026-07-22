cdf0ad31765f0bcdc96fdb77e907aa8d88bccd25
mehranniakan <65720684+mehranniakan@users.noreply.github.com>
2026-07-22 11:52:15 +0330

    some issue fix
    - delete db and migrations and recreate that again
    
    -add swagger conf to setting for JWT and Token
    
    -debug todo serializer for sending email

--- core/ToDoApp/api/v1/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from ToDoApp.api.v1.views import TaskApi
-from blog.api.v1.views import PostApi
-
-router = DefaultRouter()
-router.register('Task',TaskApi,basename='Task')
-urlpatterns = router.urls
